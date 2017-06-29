39b1c896cd1007e5ef33fc8fb040034f43650df4
Robin Cura <robin.cura@gmail.com>
2017-06-29 18:30:02 +0200

    Add agregation type : Poles (#7)

--- src_plots/Poles.R
+++ src_plots/Poles.R
@@ -7,7 +7,8 @@ output$polesNb <- renderPlot({
   ggplot(data = PolesTous, aes(factor(Annee), NbPoles)) + 
     geom_tufteboxplot() +
     xlab("Temps") + ylab("Nombre de\npôles") +
-    ggtitle("Évolution du nombre de pôles")
+    ggtitle("Évolution du nombre de pôles") +
+    labs(subtitle = "Variabilité : Réplications")
   
 })
 
@@ -21,7 +22,8 @@ output$polesNbFilter <- renderPlot({
   ggplot(data = PolesTous, aes(factor(Annee), NbPoles)) + 
     geom_tufteboxplot() +
     xlab("Temps") + ylab("Nombre de\npôles") +
-    ggtitle("Évolution du nombre de pôles")
+    ggtitle("Évolution du nombre de pôles") +
+    labs(subtitle = "Variabilité : Réplications")
   
 })
 
@@ -52,7 +54,9 @@ output$polesAgregats <- renderPlot({
     xlab("Temps") + ylab("Taux de pôles\nlocalisés dans un agrégat") +
     theme(axis.title.x=element_blank())
   
-  grid.arrange(tousPoles, polesAgregats, bottom = 'Temps')
+  grid.arrange(tousPoles, polesAgregats, bottom = 'Temps',
+               top = "Évolution de la localisation des pôles
+               Variabilité : Réplications")
   
 })
 
@@ -83,7 +87,9 @@ output$polesAgregatsFilter <- renderPlot({
     xlab("Temps") + ylab("Taux de pôles\nlocalisés dans un agrégat") +
     theme(axis.title.x=element_blank())
   
-  grid.arrange(tousPoles, polesAgregats, bottom = 'Temps')
+  grid.arrange(tousPoles, polesAgregats, bottom = 'Temps',
+               top = "Évolution de la localisation des pôles
+               Variabilité : Réplications")
   
 })
 
@@ -102,7 +108,8 @@ output$polesCompo <- renderPlot({
     xlab("Nombre d'attracteurs") +
     ylab("Fréquence") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    ggtitle("Évolution du nombre d'attracteurs des pôles")
+    ggtitle("Évolution du nombre d'attracteurs des pôles") +
+    labs(subtitle = "Variabilité : Réplications")
 
 })
 
@@ -123,7 +130,8 @@ output$polesCompoFilter <- renderPlot({
     xlab("Nombre d'attracteurs") +
     ylab("Fréquence") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    ggtitle("Évolution du nombre d'attracteurs des pôles")
+    ggtitle("Évolution du nombre d'attracteurs des pôles") +
+    labs(subtitle = "Variabilité : Réplications")
 })
 
 
@@ -142,7 +150,8 @@ output$polesAttrac <- renderPlot({
     xlab("Attractivité") +
     ylab("Fréquence") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    ggtitle("Évolution de l'attractivité des pôles")
+    ggtitle("Évolution de l'attractivité des pôles") +
+    labs(subtitle = "Variabilité : Réplications")
 })
 
 output$polesAttracFilter <- renderPlot({
@@ -162,7 +171,8 @@ output$polesAttracFilter <- renderPlot({
     xlab("Attractivité") +
     ylab("Fréquence") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    ggtitle("Évolution de l'attractivité des pôles")
+    ggtitle("Évolution de l'attractivité des pôles") +
+    labs(subtitle = "Variabilité : Réplications")
 })
 
 output$polesRT <- renderPlot({
@@ -189,7 +199,8 @@ output$polesRT <- renderPlot({
     scale_x_log10() + scale_y_log10() +
     ggtitle("Évolution rang-taille de la composition des pôles") +
     theme(legend.position = "bottom") +
-    xlab("Rang (log10)") + ylab("Nombre d'attracteurs (log10)")
+    xlab("Rang (log10)") + ylab("Nombre d'attracteurs (log10)") +
+    labs(subtitle = "Variabilité : Moyenne, Q1 et Q3 des Réplications")
 
 })
 
@@ -218,6 +229,7 @@ output$polesRTFilter <- renderPlot({
     scale_x_log10() + scale_y_log10() +
     ggtitle("Évolution rang-taille de la composition des pôles") +
     theme(legend.position = "bottom") +
-    xlab("Rang (log10)") + ylab("Nombre d'attracteurs (log10)")
+    xlab("Rang (log10)") + ylab("Nombre d'attracteurs (log10)") +
+    labs(subtitle = "Variabilité : Moyenne, Q1 et Q3 des Réplications")
 })
 
